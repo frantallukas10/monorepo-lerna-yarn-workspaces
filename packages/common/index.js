ec04c96fef40b3359cbae9c93ccc5dfe84bf9ece
FRANTL01 <lukas.frantal01@dixonscarphone.com>
2019-04-08 14:47:57 +0200

    init

---
+++ packages/common/index.js
@@ -0,0 +1,3 @@
+module.exports = () => {
+  return "hello from common";
+};
---
+++ packages/server/index.js
@@ -0,0 +1,8 @@
+const commonFunction = require("@yarn-workspace-example/common");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send(commonFunction()));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
